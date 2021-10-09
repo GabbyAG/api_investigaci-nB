62a5b862e8ade09ca8ee97253e99822761ac0d99
Gabriela <aabantog15qunc.edu.pe>
2021-10-09 01:01:18 -0500

    Terminando autenticación y acceso de servicios

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
-
-// YTfD5nAE74drMnBa ----- contraseña
 const express= require ('express');//síntaxis para importar modulos en node.js
 require('dotenv').config();
 const {dbConection} =require('./config/database');
-const cors  = require('cors');
+const cors = require('cors');
 //crear servidor
 const app=express();
-
-//Configuración de CORS
+//estableciendo la configuración de cors
 app.use(cors());
-
 //Lectura y parseo del body
 app.use(express.json());
 //crear la conexión a la BD
 dbConection();
 //verificando variables de entorno
 console.log(process.env);
-
-// Creando 
-app.get('/',(req,res)=>{
-    res.json({
-        ok:true,
-        msg: 'Bienvenidos a node'
-    });
-})
-
+//Rutas de la API
+app.use('/api/usuarios', require('./routes/usuario.routes'));
+app.use('/api/login', require('./routes/auth.routes'));
 //codigo para desplegar el servidor
 app.listen(process.env.PORT,()=>{
-    console.log('Servidor Node.js desplegado en el puerto: ' +process.env.PORT);
+    console.log('Servidor Node.js desplegado en el puerto: '+process.env.PORT);
 })
